7b1900955dd2a29d1607d80afbb8ed561a3576c8
Bowragon <ragnarokatz@gmail.com>
2020-02-26 13:32:34 -0500

    Add body and simple io components to demonstrate two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+// These two were added by the Angular CLI generator
+import { BodyComponent } from './body.component';
+import { IoComponent } from './io.component';
+
+// This app.module.ts has configuration settings for the app overall
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    IoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
