28e87b0432bde8b002b5da902ac8920412dd25a9
Bowragon <ragnarokatz@gmail.com>
2020-02-26 14:36:50 -0500

    Update to real example with actual data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home.component";
-import { AboutMeComponent } from "./about-me.component";
-import { ContactMeComponent } from "./contact-me.component";
-import { NotFoundComponent } from './not-found.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from "./contact.component";
+import { NotFoundComponent } from "./not-found.component";
+import { VehicleListComponent } from './vehicle-list.component';
+import { VehicleDetailComponent } from './vehicle-detail.component';
+import { VehicleEditComponent } from './vehicle-edit.component';
+import { VehicleDeleteComponent } from './vehicle-delete.component';
+import { VehicleCreateComponent } from './vehicle-create.component';
+// import more components here
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutMeComponent },
-  { path: 'contact', component: ContactMeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'vehicles', component: VehicleListComponent },
+  { path: 'vehicles/create', component: VehicleCreateComponent },
+  { path: 'vehicles/detail/:id', component: VehicleDetailComponent },
+  { path: 'vehicles/edit/:id', component: VehicleEditComponent },
+  { path: 'vehicles/delete/:id', component: VehicleDeleteComponent },
+  // more routes are added here
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: NotFoundComponent }  
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home.component';
-import { AboutMeComponent } from './about-me.component';
-import { ContactMeComponent } from './contact-me.component';
+import { NavComponent } from './nav.component';
 import { NotFoundComponent } from './not-found.component';
-import { NavMainComponent } from './nav-main.component';
+import { HomeComponent } from './home.component';
+import { AboutComponent } from './about.component';
+import { ContactComponent } from './contact.component';
+import { VehicleListComponent } from './vehicle-list.component';
+import { VehicleDetailComponent } from './vehicle-detail.component';
+import { VehicleEditComponent } from './vehicle-edit.component';
+import { VehicleDeleteComponent } from './vehicle-delete.component';
+import { VehicleCreateComponent } from './vehicle-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutMeComponent,
-    ContactMeComponent,
+    NavComponent,
     NotFoundComponent,
-    NavMainComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    VehicleListComponent,
+    VehicleDetailComponent,
+    VehicleEditComponent,
+    VehicleDeleteComponent,
+    VehicleCreateComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
